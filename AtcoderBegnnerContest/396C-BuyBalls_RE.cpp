7dc30903aadc8e829638420713976ef322ec1385
kchisato99 <kchisato99@gmail.com>
2025-03-10 23:44:47 +0900

    ABC396C_BuyBalls

---
+++ AtcoderBegnnerContest/396C-BuyBalls_RE.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define min(p, q) ((p) < (q) ? (p) : (q))
+#define max(p, q) ((p) > (q) ? (p) : (q))
+
+int main()
+{
+  int N, M;
+  cin >> N >> M;
+  vector<int> B(N), W(M);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> B.at(i);
+  }
+  for (int i = 0; i < M; i++)
+  {
+    cin >> W.at(i);
+  }
+
+  /*
+  N個の黒いボールとM個の白いボールがある
+  それぞれのボールには価値がつけられている
+  黒色のボールの個数が白色のボールの個数以上になるように選ぶとき
+  価値の総和としてありうる最大値を求める
+  */
+  // 黒ボールと白ボールを価値の降順にソート
+  sort(B.begin(), B.end(), greater<>());
+  cout << "test 黒: ";
+  for (int i = 0; i < N; i++)
+  {
+    cout << B.at(i) << " ";
+  }
+  cout << endl;
+  sort(W.begin(), W.end(), greater<>());
+  cout << "test 白: ";
+  for (int i = 0; i < M; i++)
+  {
+    cout << W.at(i) << " ";
+  }
+  cout << endl;
+
+  // 白色のボールをi個選んだ時の最大価値をあらかじめ計算しておく
+  vector<int> maxW(M + 1); // maxW[i]: 白色のボールをi個選んだ時の最大価値
+  //vector<int> maxBplus(N + 1);
+  maxW.at(0) = 0;
+  cout << "test 白maxW: " << maxW.at(0) << " ";
+  for (int i = 1; i <= M; i++)
+  {
+    if (W.at(i - 1) > 0)
+    {
+      maxW.at(i) = maxW.at(i - 1) + W.at(i - 1);
+    }
+    else
+    {
+      M = i - 1;
+      break;
+    }
+    cout << maxW.at(i) << " ";
+  }
+  cout << endl
+       << "test 白M: " << M << endl;
+  /*
+  MaxW[i]:白色のボールをi個選んだときの最大値
+  M:白色のボールの価値が最大の時の個数, iの最大値
+  */
+  // 黒色のボールをi個, 白色のボールをj個選んだ時の最大価値
+  vector<vector<int>> dp(N + 1, vector<int>(N + 1));
+  dp[0].at(0) = 0;
+  for (int i = 1; i <= N; i++)
+  { // 黒色のボールをi個選ぶ
+    // if (B.at(i - 1) > 0 || i <= M)
+    //{
+    // 黒色のボールをi個, 白色のボールを0個選んだ時の最大価値
+    dp[i].at(0) = dp[i - 1].at(0) + B.at(i - 1);
+    cout << "dp[" << i << "][]:" << dp[i].at(0) << " ";
+    // 黒色のボールをi個, 白色のボールをj個選んだ時の最大価値
+    for (int j = 1; j <= min(i, M); j++)
+    { // 白色のボールをj個選ぶ
+      dp[i].at(j) = dp[i].at(0) + maxW.at(min(j, M));
+      dp[0].at(i) = dp[i].at(0) + maxW.at(min(j, M));
+
+      cout << dp[i].at(j) << " ";
+    }
+    cout << endl;
+  }
+  cout << "test dp[0][i]: ";
+  for(int i = 0; i <= N; i++){
+    cout << dp[0].at(i) << " ";
+  }
+  cout << endl;
+  
+  // else
+  //{
+  //   N = i - 1;
+  //   break;
+  //}
+
+  int ans = 0;
+  for (int i = 0; i <= N; i++)
+  {
+    //for (int j = 0; j <= min(i, M); j++)
+    //{
+      if (ans < dp[0].at(i))
+      {
+        ans = dp[0].at(i);
+      }
+    //}
+  }
+  cout << ans << endl;
+}
