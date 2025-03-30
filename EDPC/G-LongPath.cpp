37e3efc1430e1fa92d4f75dbb6ce13049a16b75e
kchisato99 <kchisato99@gmail.com>
2025-03-30 15:40:45 +0900

    edpc A Frog1

--- EDPC/G-LongPath.cpp
+++ EDPC/G-LongPath.cpp
@@ -1,13 +1,22 @@
 #include <bits/stdc++.h>
-usin namespace std;
+using namespace std;
+
+int lengthSerch();
 
 int main() {
-  int N, M;
-  cin >> N >> M;
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> x(m), y(m);
+  for(int i= 0; i < m; i++) {
+    cin >> x[i] >> y[i];
+  }
 
   /*
   N頂点M辺の有向グラフG
   頂点x[i]から頂点y[i]に辺が張られている
-  Gは閉路を持たない
+  Gは閉路を含まない
   */
+
 }
+
