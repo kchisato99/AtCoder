7cf477bf564ecc5c43a38a68333e74016430c35a
kchisato99 <kchisato99@gmail.com>
2025-06-14 15:27:12 +0900

    ex22 +auto

--- APG4b/第2章/EX22.cpp
+++ APG4b/第2章/EX22.cpp
@@ -2,13 +2,40 @@
 
 using namespace std;
 
-int main() {
-int N;
-cin >> N;
+int main()
+{
+  int N;
+  cin >> N;
 
-pair<int, int> ab(N);
-for(int i = 0; i < N; i++) {
-  cin >> a[i] >> b[i];
-}
+  vector<pair<int, int>> ab(N);
+  for(auto &p : ab)
+  {
+    cin >> p.second >> p.first;
+  }
+  /*
+  for (int i = 0; i < N; i++)
+  {
+    cin >> ab[i].second >> ab[i].first;
+  }
+  /*
+    cout << endl;
+    for (int i = 0; i < N; i++)
+    {
+      cout << ab[i].first << " " << ab[i].second << endl;
+    }
+  */
+  sort(ab.begin(), ab.end());
+  /*
+  cout << endl;
+  for (int i = 0; i < N; i++)
+  {
+    cout << ab[i].second << " " << ab[i].first << endl;
+  }
+  */
 
+  //cout << endl;
+  for (auto &p : ab)
+  {
+    cout << p.second << " " << p.first << endl;
+  }
 }
