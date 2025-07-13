c4f620224d29caca7f7e14659ce7b81e707f1eb5
kchisato99 <kchisato99@gmail.com>
2025-07-13 14:17:00 +0900

    abc414

---
+++ AtcoderBeginerContest/414C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int A, N; // 基数変換 A進数に変換
+  // N 以下の整数を考える
+  cin >> A >> N;
+
+  // ----- 回文数値の生成 -----
+
+}
