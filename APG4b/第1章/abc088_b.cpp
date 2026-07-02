8187389f3bfa9f55a140011937e647f439c4e22f
kchisato99 <kchisato99@gmail.com>
2026-07-03 01:11:15 +0900

    abc088_b.c

---
+++ APG4b/第1章/abc088_b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  // n枚のカード
+  cin >> n;
+  vector<int> a(n);
+  // i枚目のカードの数字a[i]
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+
+  sort(a.begin(), a.end(), greater<int>());
+  // AliceとBobが交互にカードをとる
+  int alice = 0, bob = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (i % 2 == 0)
+    {
+      alice += a[i];
+    }
+    else
+    {
+      bob += a[i];
+    }
+  }
+  cout << alice - bob << endl;
+}
